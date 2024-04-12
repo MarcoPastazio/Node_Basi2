fb87d0c6adc3ff88b1d840f8b299dbf2d83bf289
MarcoPastazio <96430070+MarcoPastazio@users.noreply.github.com>
2024-04-12 13:35:07 +0200

    trust the process

---
+++ end_point.js
@@ -0,0 +1,29 @@
+//this is an example where i create my first end point with node and express. Let's go
+
+const express = require('express');
+
+let app = express();
+
+//i do a get and the response is a basic HELLO WORLD
+app.get("/", (req,res) => {
+    res.send("Hello World");
+});
+
+//i set the port for the comunication e after the server listen on port 3000 (for my example)
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+
+//this is an example to create a end point with node
+/*
+const http = require('http');
+
+const server = http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(uc.upperCase("Hello World!"));
+  res.end();
+}).listen(3000);
+*/
