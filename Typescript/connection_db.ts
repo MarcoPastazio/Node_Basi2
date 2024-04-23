ef198157431f9b12b5e868d081a4eb8a106ef1bf
RaffaeleDiGennaroLD <97890109+RaffaeleDiGennaroLD@users.noreply.github.com>
2024-04-23 15:43:08 +0100

    Revert "fix: fixed imports and added start script"

--- Typescript/connection_db.ts
+++ Typescript/connection_db.ts
@@ -1,10 +1,12 @@
-import { Pool } from "pg";
+const http  = require('http');
+const { Pool } = require('pg');
 
 //create one pool req
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "music_store",
-  password: "marcopastore",
+  user: 'postgres',
+  host: 'localhost',
+  database: 'music_store',
+  password: 'marcopastore',
   port: 5432,
 });
+
