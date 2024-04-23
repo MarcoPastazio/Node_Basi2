a75bbd68879fbed694ac66445ab104036e4c694b
Rafio <raffaeledigennaro10@gmail.com>
2024-04-23 15:49:01 +0100

    restored changes

--- Typescript/connection_db.ts
+++ Typescript/connection_db.ts
@@ -1,12 +1,11 @@
-const http  = require('http');
-const { Pool } = require('pg');
+import http from "http";
+import { Pool } from "pg";
 
 //create one pool req
 export const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'music_store',
-  password: 'marcopastore',
+  user: "postgres",
+  host: "localhost",
+  database: "music_store",
+  password: "marcopastore",
   port: 5432,
 });
-
