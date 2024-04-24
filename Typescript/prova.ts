a8272b2e6fc0de56d24be5917eac07563c546803
MarcoPastazio <96430070+MarcoPastazio@users.noreply.github.com>
2024-04-24 10:47:17 +0200

    change prova.ts

--- Typescript/prova.ts
+++ Typescript/prova.ts
@@ -27,6 +27,53 @@ function isNumber(x: any): x is number{
 }
 */
 
-//generics
+//overloading
 
+function saluta(nome: string, saluto?: string): string{
+    return `${saluto} ${nome}`;
+}
+
+console.log(saluta("Marco"));
+console.log(saluta("Marco", "Ue"));
+
+//overload (dichiarazione di funzione)
+function somma(a: number, b: number):number;
+function somma(a: string, b: string):string;
+//funzione da implementare
+function somma(a: any, b: any): any{
+    return a + b;
+};
+
+console.log(1+4);
+console.log("Marco", "è Brutto");
+
+
+class Cuounter{
+    contatore: number = 0;
+
+    increment(this: Cuounter) {
+        this.contatore++;
+    };
+}
+
+let counter = new Cuounter();
+counter.increment();
+counter.increment();
+
+console.log(counter.contatore);
+counter.increment();
+
+console.log(counter.contatore);
+counter.increment();
+counter.increment();
+
+console.log(counter.contatore);
+
+function listaNomi(capo: string, ...other: string[]): string{
+    return capo + " " + other.join(" ");
+}
+
+let team = listaNomi("Marco", "Pino", "KekkoTagliatella");
+
+console.log(team);
 
