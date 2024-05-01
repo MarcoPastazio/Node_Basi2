9b1bee3dc01425703ca3b505bc81e48413d01def
MarcoPastazio <pastore734@gmail.com>
2024-05-01 16:40:30 +0200

    add :

--- Javascript/end_point.js
+++ Javascript/end_point.js
@@ -12,7 +12,7 @@ app.get("/", (req,res) => {
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 //i set the port for the comunication e after the server listen on port 3000 (for my example)
